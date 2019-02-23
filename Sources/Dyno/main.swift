cf73d8d41b1d490797118d7f337ac0dca9e7ccb4
jrsonline <jrsonline@me.com>
2019-02-22 22:29:31 -0500

    Update main.swift
    Syncing with article

--- Sources/Dyno/main.swift
+++ Sources/Dyno/main.swift
@@ -1,19 +1,17 @@
 import Foundation
 import PythonKit
 
-
 let boto3 = Python.import("boto3")
-let table = boto3.Table("Dinosaurs")
+let dynamodb = boto3.resource("dynamodb")
+let table = dynamodb.Table("Dinosaurs")
+
 print( table.scan() )
 
 table.put_item(
-         Item:{
-             "id" : "1",
-             "name" : "Emojisaurus",
-             "weight" : 30,
-             "height" : 15,
-             "colour" : "blue"
-})
-
+    Item:[
+        "id" : "1",
+        "name" : "Emojisaurus",
+        "colour" : "blue"
+])
 
 print( table.scan() )
